48bb393b5fb95d9199ad27d7a8e486e5ed38a857
MehediHasan06 <mehedihasanmasud06@gmail.com>
2023-07-31 23:34:50 +0600

    2nd problem solution updated

--- interviewBit/02_highest-product.js
+++ interviewBit/02_highest-product.js
@@ -30,15 +30,14 @@ for(let i=0; i<A.length; i++){
 
 
 /**
- * * 2nd approach 
+ * * 2nd approach. This will fully complement our problem. We need to handle an edge case where 1st 2 values are negative. - >
  */
-let x = [0,-1,3,100,-70,-50].sort((a,b) => a-b);
-// let y = [1,2,3,4].sort((a,b) => a-b);
+let x = [0,-1,3,100,-70,-50].sort((a,b) => a-b); // [-70,-50,-1,0,3,100]
 
 let arrLength = x.length;
 let product1 = x[arrLength-1] * x[arrLength-2] * x[arrLength-3];
-let product2 = x[0] * x[1] * x[arrLength-1];
-let product = Math.max(product1, product2);
+let product2 = x[0] * x[1] * x[arrLength-1]; // in sorted array, if we take the first 2 then we will get +ve product.
+let product = product1 > product2 ? product1 : product2;
 
 /**
  * * Doing this for this particular array, where the sorted array will be [-70,-50,-1,0,3,100]. So, the last 3 items product will be 0.
